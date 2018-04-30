8c18e1729514307f3c0374cb2ae582548b733ba4
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-04-30 08:07:24 -0400

    react-side-effect: Fix lint issues

--- types/react-side-effect/index.d.ts
+++ types/react-side-effect/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-side-effect v1.0.2
+// Type definitions for react-side-effect 1.0
 // Project: https://github.com/gaearon/react-side-effect
 // Definitions by: Remo H. Jansen <https://github.com/remojansen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -9,15 +9,12 @@ import * as React from "react";
 declare function withSideEffect(
     reducePropsToState: (propsList: any[]) => any,
     handleStateChangeOnClient: (state: any) => any,
-    mapStateOnServer?: (state: any) => any
+    mapStateOnServer?: (state: any) => any,
 ): ClassDecorator;
 
 declare class ElementClass extends React.Component<any> {}
 
-interface ClassDecorator {
-    <T extends (typeof ElementClass)>(component:T): T;
-}
+type ClassDecorator = <T extends (typeof ElementClass)>(component: T) => T;
 
 declare namespace withSideEffect {} // https://github.com/Microsoft/TypeScript/issues/5073
-
 export = withSideEffect;
